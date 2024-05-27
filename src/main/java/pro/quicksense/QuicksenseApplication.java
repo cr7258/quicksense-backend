0476e3a4260104d5d379a268495524888de51430
chengzw <chengzw258@163.com>
2024-05-27 13:22:23 +0800

    feat: test hello world

--- src/main/java/pro/quicksense/QuicksenseApplication.java
+++ src/main/java/pro/quicksense/QuicksenseApplication.java
@@ -8,6 +8,6 @@ public class QuicksenseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(QuicksenseApplication.class, args);
-		System.out.println("Hello World");
+		System.out.println("Hello Test");
 	}
 }
